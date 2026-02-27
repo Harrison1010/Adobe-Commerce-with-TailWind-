4f6e50b7ae9e441868ef04d7f31cec968618c1d7
unknown <Harrison1010>
2026-02-28 07:24:27 +0800

    Feat:test2

--- src/utils/constData.ts
+++ src/utils/constData.ts
@@ -27,76 +27,76 @@ export const useCardData = [
 export const learnMoreData = {
     title: "Adobe commits to agentic commerce standards.",
     content: "Learn how Adobe Commerce is helping brands capture demand across AI-native shopping channels, while maintaining full control over customer relationships and brand experiences.",
-    explain:["Looking for some of the latest Adobe Commerce releases? Check out ","Adobe Commerce Optimizer"," and ","Adobe Commerce as a Cloud Service"]
+    explain: ["Looking for some of the latest Adobe Commerce releases? Check out ", "Adobe Commerce Optimizer", " and ", "Adobe Commerce as a Cloud Service"]
 }
 export const BreadCrumbData = {
-    home:"Home",
-    products:"Products",
-    adobe:"Adobe Comerce"
+    home: "Home",
+    products: "Products",
+    adobe: "Adobe Comerce"
 }
 
-export const commerceData:Array<typeCommerce>  = [
-  {
-    tabItem:"Digital storefronts",
-    title:"Lightning-fast storefronts.",
-    smmary:`Create commerce experiences that drive sales with high-performing online storefronts 
+export const commerceData: Array<typeCommerce> = [
+    {
+        tabItem: "Digital storefronts",
+        title: "Lightning-fast storefronts.",
+        smmary: `Create commerce experiences that drive sales with high-performing online storefronts 
             and generative AI-powered content creation tools.`,
-    articleItems:[`Launch your site quickly using a prebuilt storefront with integrated commerce functionality — including product listing pages, product detail pages, cart, and checkout.`
-                  ,`Increase organic traffic and conversion rates with a storefront architecture optimized to help you achieve top Google Lighthouse scores, backed by years of industry experience.`
-                  ,`Empower marketers and merchandisers to create web content in minutes using a visual storefront editor and generative AI-powered experimentation.`
-                  ,`Continually optimize your storefront with user-friendly native A/B testing tools.`],
-    buttonTitle:'Learn about digital storefronts',
-    picture:ImageNames.tab_0,
-  },
-  {
-    tabItem:"Personalized commerce",
-    title:"Real-time personalization for higher conversion rates.",
-    smmary:`Activate your commerce data to personalize every customer interaction and deliver 
+        articleItems: [`Launch your site quickly using a prebuilt storefront with integrated commerce functionality — including product listing pages, product detail pages, cart, and checkout.`
+            , `Increase organic traffic and conversion rates with a storefront architecture optimized to help you achieve top Google Lighthouse scores, backed by years of industry experience.`
+            , `Empower marketers and merchandisers to create web content in minutes using a visual storefront editor and generative AI-powered experimentation.`
+            , `Continually optimize your storefront with user-friendly native A/B testing tools.`],
+        buttonTitle: 'Learn about digital storefronts',
+        picture: ImageNames.tab_0,
+    },
+    {
+        tabItem: "Personalized commerce",
+        title: "Real-time personalization for higher conversion rates.",
+        smmary: `Activate your commerce data to personalize every customer interaction and deliver 
             seamless experiences that drive impact.`,
-    articleItems:[`Match customers to products faster with personalized search, category merchandising, and product recommendations powered by AI.`
-                  ,`Boost loyalty and sales with custom campaigns for each shopper using rich, real-time commerce data shared with other Adobe Experience Cloud solutions such as Adobe Real-Time CDP and Adobe Journey Optimizer.`
-                  ,`Activate sophisticated audiences from Real-Time CDP in Adobe Commerce to further individualize your storefront experience.`
-                  ,`Activate sophisticated audiences from Real-Time CDP in Adobe Commerce to further individualize your storefront experience.`],
-    buttonTitle:'Learn about personalized commerce',
-    picture:ImageNames.tab_1,
-  },
-  {
-    tabItem:"Composable platform",
-    title:"Simplified development. Faster time to market.",
-    smmary:`Empower your development team to innovate faster and create unique experiences with an API-first composable platform that delivers new features without upgrades and is easy to extend, integrate, and maintain.`,
-    articleItems:[`Launch quickly using a comprehensive suite of precomposed services that can be set up in minutes and are easily managed from a centralized, self-service console.`
-                  ,`Build differentiated commerce experiences with comprehensive APIs, hundreds of application events, a library of high-quality apps, and extensibility for native features and user interfaces.`
-                  ,`Extend Adobe Commerce easily with apps and services that can be installed, scaled, and updated without impacting the rest of your solution.`
-                  ,`Deliver ultra-quick response times no matter where your users are by scaling and distributing your APIs globally with edge computing.`
-                  ,'Improve developer efficiency with a low code development environment, accelerators for common use cases, and unified developer consoles, logging, and CLIs (command line interfaces).'
-                ],
-    buttonTitle:'Learn about composable development',
-    picture:ImageNames.tab_2,
-  },
-  {
-    tabItem:"B2B commerce",
-    title:"Limitless growth with scaled operations.",
-    smmary:`Operate confidently with an always up-to-date, secure, and cloud-native platform designed for enterprise scale and growth.`,
-    articleItems:[`Ensure smooth peak sales events with a distributed architecture and auto-scaling capabilities that support unlimited traffic and can process over 200,000 orders per hour.`
-                  ,`Grow your sales with a flexible catalog service that handles millions of SKUs, thousands of prices per SKU, and rapid price updates across a wide range of product types.`
-                  ,`Efficiently expand to new markets, brands, and business models — including B2C and B2B — from a single platform.`
-                  ,`Easily identify and execute new strategies for growth by analyzing commerce data using prebuilt dashboards and data visualization tools.`],
-    buttonTitle:'Learn about personalized commerce',
-    picture:ImageNames.tab_3,
-  },
-  {
-    tabItem:"Composable platform",
-    title:"A sales platform for accelerated B2B sales.",
-    smmary:`Expand your B2B business and increase revenue and loyalty with an always-on digital sales portal and tools to enhance seller productivity.`,
-    articleItems:[`Enable online B2B purchasing by supporting company accounts with multiple buyers, roles, and purchasing permissions.`
-                  ,`Personalize buying experiences with customer-specific catalogs, pricing, payment options, and promotions.`
-                  ,`Streamline purchasing through quick-ordering options and automated purchase approval processes while reducing your call volume with self-service account management.`
-                  ,`Enable your sellers to negotiate quotes entirely online and directly assist customers with configuring and completing their digital transactions.`
-                  ,'Operate a range of business models from a single platform — including B2B, direct-to-consumer, and B2B2C or B2B2B sales through channel partners.'
-                ],
-    buttonTitle:'Learn about composable development',
-    picture:ImageNames.tab_4,
-  },
+        articleItems: [`Match customers to products faster with personalized search, category merchandising, and product recommendations powered by AI.`
+            , `Boost loyalty and sales with custom campaigns for each shopper using rich, real-time commerce data shared with other Adobe Experience Cloud solutions such as Adobe Real-Time CDP and Adobe Journey Optimizer.`
+            , `Activate sophisticated audiences from Real-Time CDP in Adobe Commerce to further individualize your storefront experience.`
+            , `Activate sophisticated audiences from Real-Time CDP in Adobe Commerce to further individualize your storefront experience.`],
+        buttonTitle: 'Learn about personalized commerce',
+        picture: ImageNames.tab_1,
+    },
+    {
+        tabItem: "Composable platform",
+        title: "Simplified development. Faster time to market.",
+        smmary: `Empower your development team to innovate faster and create unique experiences with an API-first composable platform that delivers new features without upgrades and is easy to extend, integrate, and maintain.`,
+        articleItems: [`Launch quickly using a comprehensive suite of precomposed services that can be set up in minutes and are easily managed from a centralized, self-service console.`
+            , `Build differentiated commerce experiences with comprehensive APIs, hundreds of application events, a library of high-quality apps, and extensibility for native features and user interfaces.`
+            , `Extend Adobe Commerce easily with apps and services that can be installed, scaled, and updated without impacting the rest of your solution.`
+            , `Deliver ultra-quick response times no matter where your users are by scaling and distributing your APIs globally with edge computing.`
+            , 'Improve developer efficiency with a low code development environment, accelerators for common use cases, and unified developer consoles, logging, and CLIs (command line interfaces).'
+        ],
+        buttonTitle: 'Learn about composable development',
+        picture: ImageNames.tab_2,
+    },
+    {
+        tabItem: "B2B commerce",
+        title: "Limitless growth with scaled operations.",
+        smmary: `Operate confidently with an always up-to-date, secure, and cloud-native platform designed for enterprise scale and growth.`,
+        articleItems: [`Ensure smooth peak sales events with a distributed architecture and auto-scaling capabilities that support unlimited traffic and can process over 200,000 orders per hour.`
+            , `Grow your sales with a flexible catalog service that handles millions of SKUs, thousands of prices per SKU, and rapid price updates across a wide range of product types.`
+            , `Efficiently expand to new markets, brands, and business models — including B2C and B2B — from a single platform.`
+            , `Easily identify and execute new strategies for growth by analyzing commerce data using prebuilt dashboards and data visualization tools.`],
+        buttonTitle: 'Learn about personalized commerce',
+        picture: ImageNames.tab_3,
+    },
+    {
+        tabItem: "Composable platform",
+        title: "A sales platform for accelerated B2B sales.",
+        smmary: `Expand your B2B business and increase revenue and loyalty with an always-on digital sales portal and tools to enhance seller productivity.`,
+        articleItems: [`Enable online B2B purchasing by supporting company accounts with multiple buyers, roles, and purchasing permissions.`
+            , `Personalize buying experiences with customer-specific catalogs, pricing, payment options, and promotions.`
+            , `Streamline purchasing through quick-ordering options and automated purchase approval processes while reducing your call volume with self-service account management.`
+            , `Enable your sellers to negotiate quotes entirely online and directly assist customers with configuring and completing their digital transactions.`
+            , 'Operate a range of business models from a single platform — including B2B, direct-to-consumer, and B2B2C or B2B2B sales through channel partners.'
+        ],
+        buttonTitle: 'Learn about composable development',
+        picture: ImageNames.tab_4,
+    },
 ]
 
 
@@ -209,8 +209,8 @@ export const contentsData = [
     },
 ]
 export const registerData = {
-    title:"See the future of marketing, creativity and AI.",
-    content:"Join us in Las Vegas, April 19-22. "
+    title: "See the future of marketing, creativity and AI.",
+    content: "Join us in Las Vegas, April 19-22. "
 }
 export const startedData = {
     image: ImageNames.b2b,
@@ -221,31 +221,31 @@ export const startedData = {
 }
 export const BrandData = [
     {
-        image:ImageNames.mark1,
-        title:"89%",
+        image: ImageNames.mark1,
+        title: "89%",
         description: "conversion rate among re-engaged shoppers"
     },
     {
-        image:ImageNames.mark2,
-        title:"41%",
+        image: ImageNames.mark2,
+        title: "41%",
         description: "conversion rate lift by applying personalization"
     },
     {
-        image:ImageNames.mark3,
-        title:"3x faster",
+        image: ImageNames.mark3,
+        title: "3x faster",
         description: "page load times."
     },
     {
-        image:ImageNames.mark4,
-        title:"44%",
+        image: ImageNames.mark4,
+        title: "44%",
         description: "increased site visits"
     }
 ]
 export const integrationData = {
-    image:ImageNames.integration,
-    width:500,
-    height:333,
-    title:"Extend your Adobe Commerce capabilities even further.",
+    image: ImageNames.integration,
+    width: 500,
+    height: 333,
+    title: "Extend your Adobe Commerce capabilities even further.",
     content: "Integrate Adobe Commerce with your existing tech stack to enhance creativity, personalize experiences, and connect data across channels.",
     buttontext: "Go to Integrations"
 }
@@ -259,34 +259,34 @@ export const reportData = {
 }
 export const commerceItemData = [
     {
-        icon:ImageNames.webniar,
-        title:'Webinar',
-        content:'Adobe Commerce: Building the Experience-Led Commerce Platform'
+        icon: ImageNames.webniar,
+        title: 'Webinar',
+        content: 'Adobe Commerce: Building the Experience-Led Commerce Platform'
     },
     {
-        icon:ImageNames.caseStudy,
-        title:'Customer',
-        content:`Signify increases net merchandized value by 55%.`
+        icon: ImageNames.caseStudy,
+        title: 'Customer',
+        content: `Signify increases net merchandized value by 55%.`
     },
     {
-        icon:ImageNames.eventSession,
-        title:'Event session',
-        content:'Adobe Commerce 2025 product roadmap review.'
+        icon: ImageNames.eventSession,
+        title: 'Event session',
+        content: 'Adobe Commerce 2025 product roadmap review.'
     },
     {
-        icon:ImageNames.guide,
-        title:'Guide',
-        content:'Five must-haves for your B2B and B2C commerce platform.'
+        icon: ImageNames.guide,
+        title: 'Guide',
+        content: 'Five must-haves for your B2B and B2C commerce platform.'
     },
     {
-        icon:ImageNames.guide,
-        title:'guide',
-        content:'Getting started with Adobe Commerce.'
+        icon: ImageNames.guide,
+        title: 'guide',
+        content: 'Getting started with Adobe Commerce.'
     },
     {
-        icon:ImageNames.eventSession,
-        title:'Event session',
-        content:`HanesBrands creates the world's fastest storefront.`
+        icon: ImageNames.eventSession,
+        title: 'Event session',
+        content: `HanesBrands creates the world's fastest storefront.`
     },
 ]
 export const TabsTitle = "Explore the features and capabilities of Adobe Commerce.";
@@ -294,3 +294,4 @@ export const ContractTitle = "Let us show you the full power of Adobe Commerce."
 export const adobeTitle = "Dive deeper into Adobe Commerce.";
 export const adverTitle = "Adobe drives global sales for top brands.";
 export const usecaseTitle = "Adobe Commerce use CaseTitle.";
+export const testData_2 = "test_2";
